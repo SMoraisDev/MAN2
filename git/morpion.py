4a2a4977b6d299f8aa8ce1800576e1797a12eb09
SMoraisDev <sebastien.morais@proton.me>
2024-10-10 16:00:43 +0200

    FEATURE: Add morpion

---
+++ git/morpion.py
@@ -0,0 +1,27 @@
+class Morpion:
+
+    def __init__(self):
+        self.__plateau = [["","",""],["","",""],["","",""]]
+        self.__joueur = "X"
+
+    def afficher(self):
+        pass
+
+    def jouer_coup(self):
+        pass
+
+    def verifie_victoire(self):
+        pass
+
+    def plateau_plein(self):
+        pass
+
+    def chager_joueur(self):
+        pass
+
+    def jouer(self):
+        pass
+
+if __name__ == "__main__":
+    m = Morpion
+    m.jouer()
